946e516fa23ac29dafe705b5be01c9d8e201e0f9
Alex Harrison <alexjharrison@gmail.com>
2025-02-08 18:50:46 -0500

    open dev server to local network

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,6 +5,10 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
+  devServer: {
+    host: "0.0.0.0",
+    port: 3000,
+  },
   nitro: {
     experimental: {
       database: true,
